aad628ad15cb125e1f1ff4982b08fde7cdd6f759
Игорь <42812135+igor228337@users.noreply.github.com>
2024-01-16 22:07:31 +0300

    Update config.py

--- TG/config.py
+++ TG/config.py
@@ -1,7 +1,7 @@
 BOT_TOKEN = "6437988250:AAGJRvRVjLseX9WSOTcwxAISyI7fYCMamI8"
 id_admin = 6044110141
-email_address = "dramaturg2021igor@yandex.ru"
-email_password = "Vfnhtif1"
+email_address = ""
+email_password = ""
 host = "smtp.yandex.com" # если yandex
 # host = "smtp.mail.ru" # если mail и так далее
 BASE_PROVIDERS = []
